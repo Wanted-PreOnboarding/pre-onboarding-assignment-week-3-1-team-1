d427057eb5d9703708608ecf26e9af5f0ee3b7a2
unsnruu <unsnruu@gmail.com>
2022-09-13 19:59:01 +0900

    [#3] feat : ErrorBoundary 컴포넌트 생성
    에러를 핸들링하기 위한 ErrorBoundary 컴포넌트를 생성하였습니다.

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const initialState = { error: null };
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  static getDerivedStateFromError(error) {
+    // 다음 렌더링에서 폴백 UI가 보이도록 상태를 업데이트 합니다.
+    return { error };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // 에러 리포팅 서비스에 에러를 기록할 수도 있습니다.
+    this.props?.onError?.(error, errorInfo);
+  }
+
+  render() {
+    const { error } = this.state;
+    const { FallbackComponent } = this.state;
+
+    if (error !== null) {
+      const props = { error };
+
+      if (FallbackComponent) {
+        return <FallbackComponent {...props} />;
+      } else {
+        throw new Error('ErrorBoundary는 FallbackComponent가 필요합니다.');
+      }
+    }
+
+    return this.props.children;
+  }
+}
