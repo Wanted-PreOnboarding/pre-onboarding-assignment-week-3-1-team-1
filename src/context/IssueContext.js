0ffa3a8d00f59e5abeea911685b87f9d00f4ad9e
jungsoo <sunpl13@naver.com>
2022-09-14 14:24:25 +0900

    [#10] feat : Issue Reducer 생성

--- src/context/IssueContext.js
+++ src/context/IssueContext.js
@@ -30,3 +30,40 @@ const error = error => ({
   data: null,
   error: error,
 });
+// reducer
+function IssuesReducer(state, action) {
+  switch (action.type) {
+    case 'GET_ISSUES_PENDING':
+      return {
+        ...state,
+        issues: loadingState,
+      };
+    case 'GET_ISSUES_SUCCESS':
+      return {
+        ...state,
+        issues: success(action.data),
+      };
+    case 'GET_ISSUES_ERROR':
+      return {
+        ...state,
+        issues: error(action.error),
+      };
+    case 'GET_ISSUES_DETAIL_PENDING':
+      return {
+        ...state,
+        issueDetail: loadingState,
+      };
+    case 'GET_ISSUES_DETAIL_SUCCESS':
+      return {
+        ...state,
+        issueDetail: success(action.data),
+      };
+    case 'GET_ISSUES_DETAIL_ERROR':
+      return {
+        ...state,
+        issueDetail: error(action.error),
+      };
+    default:
+      return state;
+  }
+}
