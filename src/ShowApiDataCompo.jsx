bc1a57792ae84cce52157771301a8566dadfdf7d
unsnruu <unsnruu@gmail.com>
2022-09-14 14:48:35 +0900

    [#11] docs : 주석으로 에러 핸들링 방법 명시
    주석으로 에러 핸들링을 하는 또 다른 방법을 명시하였습니다.

--- src/ShowApiDataCompo.jsx
+++ src/ShowApiDataCompo.jsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { useIssuesState, useIssuesDispatch } from './context/IssueContext';
+// import {useErrorHandler} from './components/ErrorBoundary'
 
+// !!!
 // api 연결 확인 되었는지 보는 컴포넌트입니다.
 // 이슈목록 화면입니다.
 // 프로젝트 제출 전에 해당 컴포넌트는 삭제하겠습니다.
@@ -13,6 +15,7 @@ const ShowApiDataCompo = () => {
   const [list, setList] = useState(null);
   const state = useIssuesState();
   const dispatch = useIssuesDispatch();
+  // const handleError = useErrorHandler();
 
   useEffect(() => {
     async function getApi() {
@@ -33,6 +36,8 @@ const ShowApiDataCompo = () => {
 
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
+  //에러 핸들링 대안입니다. 위에 주석처리된 handleError와 import 구문의 추석을 해제하고 시도해 보세요.
+  // if (error) return handleError(error);
   if (!data) return null; //여기 커스텀해서 무한 스크롤 구현해주시면 됩니다.
 
   return (
