be6d8ae8cffe0fbf1e9ff23405137c98e6127720
jungsoo <sunpl13@naver.com>
2022-09-14 14:27:19 +0900

    [#10] feat : App Component Context Provider로 감싸기

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router';
 
 import './App.css';
 import { AppContainer } from './styles/reset';
-
+import { IssuesProvider } from './context/IssueContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ShowApiDataCompo from './ShowApiDataCompo';
@@ -14,14 +14,16 @@ import Fallback from './components/Fallback';
 function App() {
   return (
     <AppContainer className="App">
-      <Header />
-      <ErrorBoundary Fallback={<Fallback />}>
-        <Routes>
-          <Route path="/" element={<ShowApiDataCompo />} />
-          <Route path="/detail" element={<ShowApiDetailCompo />} />
-        </Routes>
-      </ErrorBoundary>
-      <Footer />
+      <IssuesProvider>
+        <Header />
+        <ErrorBoundary Fallback={<Fallback />}>
+          <Routes>
+            <Route path="/" element={<ShowApiDataCompo />} />
+            <Route path="/detail" element={<ShowApiDetailCompo />} />
+          </Routes>
+        </ErrorBoundary>
+        <Footer />
+      </IssuesProvider>
     </AppContainer>
   );
 }
