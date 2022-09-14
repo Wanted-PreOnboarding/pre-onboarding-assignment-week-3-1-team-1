1cf486c2e6888178cf5a8034d5e4ebc860b05d9f
kunnie-code <kearnyyoon@gmail.com>
2022-09-14 15:40:51 +0900

    [#4] feat : 무한 스크롤 구현

--- src/ShowApiDataCompo.jsx
+++ src/ShowApiDataCompo.jsx
@@ -9,22 +9,40 @@ import { useNavigate } from 'react-router-dom';
 // 프로젝트 제출 전에 해당 컴포넌트는 삭제하겠습니다.
 const ShowApiDataCompo = () => {
   const navigate = useNavigate();
-  const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+  const [isFetching, setFetching] = useState(false);
+
+  async function getApi(page) {
+    const fecthData = await ApiModel.getList(page);
+    console.info(fecthData);
+    setData(data.concat(fecthData));
+    setPage(page + 1);
+    setFetching(false);
+  }
 
   useEffect(() => {
-    async function getApi() {
-      const data = await ApiModel.getList();
-      console.info(data);
-      setData(data);
-    }
-
-    getApi();
+    getApi(page);
+    const handleScroll = () => {
+      const { scrollTop, offsetHeight } = document.documentElement;
+      if (window.innerHeight + scrollTop >= offsetHeight) {
+        setFetching(true);
+      }
+    };
+    setFetching(true);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   const goToDetailPage = e => {
     const num = e.target.value;
     navigate('/detail', { state: { num } });
   };
+
+  useEffect(() => {
+    if (isFetching) getApi(page);
+    else setFetching(false);
+  }, [isFetching]);
 
   return (
     <Box className="example-box">
@@ -56,6 +74,7 @@ const ShowApiDataCompo = () => {
           </button>
         </div>
       ))}
+      {isFetching && <h1>로딩중...</h1>}
     </Box>
   );
 };
