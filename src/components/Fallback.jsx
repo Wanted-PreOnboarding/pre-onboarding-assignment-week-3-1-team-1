2fe50c39f25f2bbb413653fe4627af4325fd0d5b
unsnruu <unsnruu@gmail.com>
2022-09-15 11:16:31 +0900

    [#31] style : Fallback 컴포넌트 flex로 감싸기
    Fallback 컴포넌트를 flex로 감쌌습니다.

--- src/components/Fallback.jsx
+++ src/components/Fallback.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Fallback() {
   return (
-    <div>
-      <h2>SAMPLE FALLBACK</h2>
-      <p>문제가 발생했어요</p>
-    </div>
+    <Container>
+      <h2>문제가 발생했습니다</h2>
+    </Container>
   );
 }
 
 export default Fallback;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
