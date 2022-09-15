be50e063faaad23fae2ce38a64bad9a33fdfc00f
unsnruu <unsnruu@gmail.com>
2022-09-15 11:34:27 +0900

    [#] style : 문제 리포트 링크 추가
    문제가 발생했을 시에 문제를 리포트 할 수 있는 링크를 추가하였습니다.

--- src/components/Fallback.jsx
+++ src/components/Fallback.jsx
@@ -5,6 +5,9 @@ function Fallback() {
   return (
     <Container>
       <h2>문제가 발생했습니다</h2>
+      <a href="https://github.com/wanted-fe-6/wanted-pre-onboarding-fe-6-3-1/issues">
+        저희에게 문제를 알려주세요
+      </a>
     </Container>
   );
 }
@@ -16,4 +19,8 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
+  & a {
+    text-decoration: none;
+  }
 `;
