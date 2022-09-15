b6305d91067ad6fc6d362a68ccfbf2b669e15043
unsnruu <unsnruu@gmail.com>
2022-09-15 11:50:09 +0900

    [#31] feat : 스타일링 및 에러를 보여도록 수정
    에러를 보여주도록 수정하고 스타일링을 더했습니다.

--- src/components/Fallback.jsx
+++ src/components/Fallback.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropType from 'prop-types';
 
-function Fallback() {
+function Fallback({ error }) {
+  const { message } = error;
   return (
     <Container>
       <h2>문제가 발생했습니다</h2>
+      <pre>
+        <span>오류 메세지</span>
+        <br />
+        {message}
+      </pre>
       <a href="https://github.com/wanted-fe-6/wanted-pre-onboarding-fe-6-3-1/issues">
         저희에게 문제를 알려주세요
       </a>
@@ -19,8 +26,19 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-
+  & pre {
+    background-color: rgba(0, 0, 0, 0.3);
+    padding: 1rem;
+    border-radius: 0.2rem;
+  }
+  & > pre > span {
+    font-weight: bold;
+  }
   & a {
     text-decoration: none;
   }
 `;
+
+Fallback.propTyes = {
+  error: PropType.object,
+};
